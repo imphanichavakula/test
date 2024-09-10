3105783a526541e78bbaffb61b84c5632a03c027
imphanichavakula <phani.chavakula@gmail.com>
2024-09-10 23:50:15 +0530

    Update filqe.sh

--- file.sh
+++ file.sh
@@ -1,6 +1,4 @@
-echo "This is GITHUB and Jenkins Integration"
-echo "Looks Cool!"
-echo "test is done"
-echo "Lahari made change in github and code is pushed"
+echo "This is GITHUB and Jenkins Integratio\ imn"
+
 
 
